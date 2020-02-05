0dddb76ca29fd94e79a81541010ebf6082ffa4a9
Nicolas Schäfli <ns@studer-raimann.ch>
2020-02-05 16:39:26 +0100

    Use build target buildPath for output base directory

--- builders/obfuscate/index.ts
+++ builders/obfuscate/index.ts
@@ -1,5 +1,6 @@
 import { BuilderContext, BuilderOutput, createBuilder } from "@angular-devkit/architect";
-import { JsonObject } from "@angular-devkit/core";
+import { JsonObject, workspaces } from "@angular-devkit/core";
+import { NodeJsSyncHost } from "@angular-devkit/core/node";
 import { mkdir, readFile, writeFile } from "fs";
 import { obfuscate, ObfuscatedCode } from "javascript-obfuscator";
 import { dirname, join, normalize } from "path";
@@ -7,6 +8,7 @@ import readdir from "readdirp";
 import { promisify } from "util";
 import { JsFiles, ObfuscateCommandOptions } from "./options";
 import { transfer } from "multi-stage-sourcemap";
+
 
 const preadFile = promisify(readFile);
 const pwriteFile = promisify(writeFile);
@@ -39,7 +41,7 @@ async function _commandBuilder(options: ObfuscateCommandOptions & JsonObject, co
 
 async function obfuscateJsFiles(context: BuilderContext, options: ObfuscateCommandOptions & JsonObject, files: JsFiles): Promise<void> {
     const inputPath: string = join(context.currentDirectory, files.input);
-    const outputPath: string = join(context.currentDirectory, files.output);
+    const outputPath: string = join(context.currentDirectory, (await getBuildPath(context)), files.output);
     for await (const item of readdir(inputPath, {fileFilter: files.glob, type: "files", depth: 30})) {
         if (processedFiles.has(item.fullPath)) {
             continue;
@@ -68,6 +70,28 @@ async function obfuscateJsFiles(context: BuilderContext, options: ObfuscateComma
         await Promise.all(jobs);
         processedFiles.add(item.fullPath);
     }
+}
+
+async function getBuildPath(context: BuilderContext): Promise<string> {
+    const host = workspaces.createWorkspaceHost(new NodeJsSyncHost());
+    const workspace = await workspaces.readWorkspace(context.currentDirectory, host);
+
+    const project = workspace.workspace.projects.get(context.target.project);
+    if (!project) {
+        throw new Error(`${context.target.project} not found.`);
+    }
+
+    const buildTarget = project.targets.get("build");
+    if (!buildTarget) {
+        throw new Error("Build target does not exist!");
+    }
+
+    const outputBasePath: unknown = buildTarget.configurations?.production?.outputPath ?? buildTarget.options?.outputPath;
+    if (typeof outputBasePath !== "string") {
+        throw new Error("Build target has no build directory configured!");
+    }
+
+    return outputBasePath;
 }
 
 export default createBuilder(_commandBuilder);
